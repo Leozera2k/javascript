a2b12559d605f46ee936183291647008e8266b3b
Leozera2k <leofsousa98@gmail.com>
2024-07-11 12:27:00 -0300

    Update ex013.js

--- curso-em-video/aula12/ex013.js
+++ curso-em-video/aula12/ex013.js
@@ -1,6 +1,7 @@
 var agora = new Date()
 var diaSem = agora.getDay()
 /*
+
  0 = Domingo
  1 = Segunda
  2 = Terça 
@@ -8,7 +9,7 @@ var diaSem = agora.getDay()
  4 = Quinta
  5 = Sexta
  6 = Sábado
-
+ 
 */
 
 console.log(diaSem)
