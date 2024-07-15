631a683f8d305f166d60769ea18ef3ef276abdb3
Leozera2k <leofsousa98@gmail.com>
2024-07-15 14:56:03 -0300

    Update script.js

--- curso-em-video/aula12ex2/script.js
+++ curso-em-video/aula12ex2/script.js
@@ -27,25 +27,31 @@ function verificar(){
                 img.setAttribute('src', 'adulto-m.png')
             }
             else {
-
+                img.setAttribute('src', 'idoso-m.png')
             }
         }
         else if (fsex[1].checked){
             genero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
+                img.setAttribute('src', 'bebe-f.png')
             }
-            else if (idade >= 10 && idade < 21) {
+            else if (idade < 21) {
                 // Jovem
+                img.setAttribute('src', 'jovem-f.png')
             }
             else if(idade < 50) {
                 //Adulto
+                img.setAttribute('src', 'adulto-f.png')
             }
             else {
                 //idoso
+                img.setAttribute('src', 'idoso-f.png')
             }
+           
         }
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
