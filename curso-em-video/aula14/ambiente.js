9321c9886c104aa9cc6c2431137e74382025d9e1
Leozera2k <leofsousa98@gmail.com>
2024-07-15 15:27:10 -0300

    update ambiente.js

--- curso-em-video/aula14/ambiente.js
+++ curso-em-video/aula14/ambiente.js
@@ -1,4 +1,11 @@
 /*
 Repetição For:
     for ( inicio ; teste ; incremento)
+    
+    Esse tipo de repetição é usado quando sabemos o intervalo de quantas vezes precisamos da repetição
 */
+console.log('Vai começar!')
+for (var c = 1; c <=10; c++){
+    console.log(`Passo ${c}`)
+}
+console.log('Fim!')
